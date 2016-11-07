ebabb4aae829e8e752732a3a0818a810b7f7a60b
Jay Lee <kjaylee@Jayui-MacBook-Pro.local>
2016-11-08 02:51:49 +0900

    commit

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def charm():
+    return 'Hello Charm!'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
